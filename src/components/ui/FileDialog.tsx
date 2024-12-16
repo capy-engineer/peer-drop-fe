4c6cf543578668db0d2eed342a5e6574ae54e669
hxuan190 <hxuan190@gmail.com>
2024-12-16 21:42:30 +0700

    Refactor FileDialog component to establish WebSocket connection on mount and improve layout with QR code display

--- src/components/ui/FileDialog.tsx
+++ src/components/ui/FileDialog.tsx
@@ -19,37 +19,33 @@ interface FileDialogProps {
 
 export default function FileDialog({ files, open, setOpen }: FileDialogProps) {
   const [uuid, setUuid] = useState<string | null>(null);
-  const [isSharing, setIsSharing] = useState(false);
 
   useEffect(() => {
-    let socket: WebSocket;
+    let socket: WebSocket | null = null;
 
-    if (isSharing) {
-      const apiHost = process.env.NEXT_PUBLIC_WS_URL;
-      console.log(apiHost);
-      if (apiHost) {
-        socket = new WebSocket(apiHost);
-        socket.onopen = () => {
-          console.log("Socket connection established");
-        };
+    const apiHost = process.env.NEXT_PUBLIC_WS_URL;
+    if (apiHost) {
+      socket = new WebSocket(apiHost);
+      socket.onopen = () => {
+        console.log("Socket connection established");
+      };
 
-        socket.onmessage = (event) => {
-          const data = JSON.parse(event.data);
-          if (data.peerId) {
-            setUuid(data.peerId);
-          }
-        };
+      socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        if (data.peerId) {
+          setUuid(data.peerId);
+        }
+      };
 
-        socket.onerror = (error) => {
-          console.error("WebSocket error:", error);
-        };
+      socket.onerror = (error) => {
+        console.error("WebSocket error:", error);
+      };
 
-        socket.onclose = () => {
-          console.log("WebSocket connection closed");
-        };
-      } else {
-        console.error("API_HOST_1 is not defined");
-      }
+      socket.onclose = () => {
+        console.log("WebSocket connection closed");
+      };
+    } else {
+      console.error("API_HOST_1 is not defined");
     }
 
     return () => {
@@ -57,7 +53,7 @@ export default function FileDialog({ files, open, setOpen }: FileDialogProps) {
         socket.close();
       }
     };
-  }, [isSharing]);
+  }, [uuid]);
 
   return (
     <div className="flex justify-center mt-4">
@@ -77,18 +73,30 @@ export default function FileDialog({ files, open, setOpen }: FileDialogProps) {
             </DialogDescription>
           </DialogHeader>
           <div className="py-4">
-            <p>Total files: {files.length}</p>
-            <p>
-              Total size:{" "}
-              {(
-                files.reduce((acc, file) => acc + file.size, 0) /
-                (1024 * 1024)
-              ).toFixed(2)}{" "}
-              MB
-            </p>
             {uuid && (
-              <div className="mt-4 text-center">
-                <QRCodeSVG value={uuid} size={256} />
+              <div className="mt-4 flex flex-col items-center">
+                <p>Total files: {files.length}</p>
+                <p className="mb-4">
+                  Total size:{" "}
+                  {(
+                    files.reduce((acc, file) => acc + file.size, 0) /
+                    (1024 * 1024)
+                  ).toFixed(2)}{" "}
+                  MB
+                </p>
+                <QRCodeSVG
+                  value={uuid}
+                  size={256}
+                  imageSettings={{
+                    src: "https://picsbed.top/file/fhGovySlxNBNw%2FSC%2F%2FeAzE5Snn8RHj6GnKHzyWP36fQ%3D",
+                    x: undefined,
+                    y: undefined,
+                    height: 80,
+                    width: 80,
+                    opacity: 1,
+                    excavate: true,
+                  }}
+                />
                 <div className="mt-4 flex items-center justify-center gap-2">
                   <input
                     type="text"
@@ -103,22 +111,8 @@ export default function FileDialog({ files, open, setOpen }: FileDialogProps) {
                         `https://your-site.com/${uuid}`
                       );
                     }}
-                    className="gap-2"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="size-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
-                      />
-                    </svg>
+                    Copy
                   </Button>
                 </div>
               </div>
@@ -128,15 +122,6 @@ export default function FileDialog({ files, open, setOpen }: FileDialogProps) {
             <Button variant="outline" onClick={() => setOpen(false)}>
               Cancel
             </Button>
-            {!uuid && (
-              <Button
-                onClick={() => {
-                  setIsSharing(true); // Trigger WebSocket connection
-                }}
-              >
-                Share
-              </Button>
-            )}
           </DialogFooter>
         </DialogContent>
       </Dialog>
